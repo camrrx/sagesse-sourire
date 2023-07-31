5a564c8645712187493ec114f2f60044d704b1fa
Camille Roux <Camille@MacBook-Pro-de-MacBook.local>
2023-07-31 17:34:27 +0200

    menu with headless ui

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,31 +1,50 @@
 import "./Navbar.scss";
 import { menuItems } from "../../assets/menuItems";
 import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Menu } from "@headlessui/react";
 
 const Navbar = () => {
+	const [showNavbar, setShowNavbar] = useState(false);
+
+	const toggleNavbar = () => {
+		setShowNavbar(!showNavbar);
+	};
+
 	return (
-		<nav>
-			<ul className="navbar">
-				{menuItems.map((item, index) => (
-					<li key={index} className="navbar-item">
-						<Link to={item.url} className="navbar-link">
-							{item.title}
-						</Link>
-						{item.submenu && (
-							<ul className="submenu">
-								{item.submenu.map((subItem, subIndex) => (
-									<li key={subIndex} className="submenu-item">
-										<Link to={subItem.url} className="submenu-link">
-											{subItem.title}
-										</Link>
-									</li>
-								))}
-							</ul>
-						)}
-					</li>
-				))}
-			</ul>
-		</nav>
+		<div>
+			<button className="navbar-toggle" onClick={toggleNavbar}>
+				Navbar
+			</button>
+			<nav>
+				<ul className="navbar">
+					{menuItems.map((item, index) => (
+						<li key={index} className="navbar-item">
+							<Menu>
+								<Menu.Button className="navbar-link">{item.title}</Menu.Button>
+								{item.submenu && (
+									<Menu.Items className="submenu">
+										{item.submenu.map((subItem, subIndex) => (
+											<Menu.Item key={subIndex}>
+												{({ active }) => (
+													<Link
+														to={subItem.url}
+														className={`submenu-link ${
+															active ? "active" : ""
+														}`}>
+														{subItem.title}
+													</Link>
+												)}
+											</Menu.Item>
+										))}
+									</Menu.Items>
+								)}
+							</Menu>
+						</li>
+					))}
+				</ul>
+			</nav>
+		</div>
 	);
 };
 
