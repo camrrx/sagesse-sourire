2ceed945703e93444c69a1482a2784c9ad72cf8a
Camille Roux <Camille@MacBook-Pro-de-MacBook.local>
2023-08-01 19:07:16 +0200

    presentation

---
+++ src/Components/Presentation/Presentation.tsx
@@ -0,0 +1,39 @@
+import "./Presentation.scss";
+import dentiste from "../../assets/dentiste.jpeg";
+const Presentation = () => {
+	return (
+		<div className="presentation-container">
+			<div className="titre">
+				<h4 className="presentation-texte">
+					Au service de votre bien-être et de votre santé.
+				</h4>
+				<h2 className="presentation-titre">
+					Nous sommes les garants d'une dentisterie moderne et actualisée.
+				</h2>
+			</div>
+			<div className="presentation-sagesse">
+				<div className="titre1">
+					<h5>A chateaubriant depuis</h5>
+					<h2>1903</h2>
+				</div>
+				<img src={dentiste} className="imgdentiste" />
+				<div className="description1">
+					<h5>
+						Notre équipe médicale s’est regroupée autour de compétences
+						professionnelles et humaines qui permettent une prise en charge et
+						une qualité de soins de haut niveau, dans tous les domaines de la
+						chirurgie dentaire. Cet exercice de groupe permet de mettre en
+						commun les compétences et qualités de chacun au profit de tous.
+					</h5>
+					<h6>
+						Toute notre équipe s’engage à informer les patients sur les
+						avantages et les inconvénients éventuels des traitements réalisés et
+						sur leurs coûts.
+					</h6>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Presentation;
--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -2,21 +2,26 @@ import "./Home.scss";
 import Navbar from "../../Components/Navbar/Navbar";
 import Bienvenue from "../../Components/Bienvenue/Bienvenue";
 import Actualite from "../../Components/Actualite/Actualite";
+import Presentation from "../../Components/Presentation/Presentation";
 
 const Home = () => {
-  return (
-    <div className="home-container light-theme">
-      <div className="navbar">
-        <Navbar />
-      </div>
-      <div className="bienvenue">
-        <Bienvenue></Bienvenue>
-      </div>
-      <div className="actualite">
-        <Actualite></Actualite>
-      </div>
-    </div>
-  );
+	return (
+		<div className="home-container light-theme">
+			<div className="navbar">
+				<Navbar />
+			</div>
+			<div className="bienvenue">
+				<Bienvenue></Bienvenue>
+			</div>
+			<div className="presentation">
+				<Presentation></Presentation>
+			</div>
+			<div className="separation"></div>
+			<div className="actualite">
+				<Actualite></Actualite>
+			</div>
+		</div>
+	);
 };
 
 export default Home;
