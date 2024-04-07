6c1b3aa2e9ddc3767fe743066f7b97ea47b71e62
Camille Roux <Camille@MacBook-Pro-de-MacBook.local>
2024-04-07 18:38:56 +0200

    ortho done

--- src/Components/TraitementsComponents/Orthodontie/Orthodontie.tsx
+++ src/Components/TraitementsComponents/Orthodontie/Orthodontie.tsx
@@ -37,16 +37,16 @@ const Orthodontie = () => {
 							<div>
 								<h2>Il existe deux types de brackets :</h2>
 								<div className="bracket-container">
-									<div className="bracket">
+									<div className="card">
 										<h4>Bracket métallique</h4>
 										<img className="ortho-img" src={bracketMetal}></img>
 									</div>
-									<div className="bracket">
+									<div className="card">
 										<h4>Bracket céramique</h4>
 										<img className="ortho-img" src={bracketCeramique}></img>
 									</div>
 								</div>
-								<h5>
+								<h6 className="card">
 									Si on observe, un chevauchement dentaire ou une mauvaise
 									relation des maxillaires, il est conseillé de consulter un
 									dentiste pour savoir si un traitement orthodontique est
@@ -55,59 +55,52 @@ const Orthodontie = () => {
 									l’orthodontiste. Cet examen doit inclure la prise d'empreintes
 									des dents du haut et du bas, de photographies des dents et du
 									visage, et de radiographies des mâchoires incluant les dents
-									et de la tête au complet.
-								</h5>
-								<h6>
-									En moyenne, un traitement orthodontique complet peut durer de
-									1 à 3 ans. Les patients doivent porter après la fin du
-									traitement un mainteneur d’espace pendant au moins 6 mois.
-									Suite aux ajustements fait durant le traitement d'orthodontie,
-									un inconfort ou une douleur peut être sentie au niveau des
-									dents et de la bouche. Si une douleur persiste il faut en
-									parler à l'orthodontiste. Le processus mécanique est le même à
-									n'importe quel âge. Pour votre cas, notre équipe s’engage à
-									vous informer sur les avantages et les inconvénients éventuels
-									des traitements à réalisés et sur leurs coûts.
+									et de la tête au complet. <br /> En moyenne, un traitement
+									orthodontique complet peut durer de 1 à 3 ans. Les patients
+									doivent porter après la fin du traitement un mainteneur
+									d’espace pendant au moins 6 mois. Suite aux ajustements fait
+									durant le traitement d'orthodontie, un inconfort ou une
+									douleur peut être sentie au niveau des dents et de la bouche.
+									Si une douleur persiste il faut en parler à l'orthodontiste.
+									Le processus mécanique est le même à n'importe quel âge. Pour
+									votre cas, notre équipe s’engage à vous informer sur les
+									avantages et les inconvénients éventuels des traitements à
+									réalisés et sur leurs coûts.
 								</h6>
-								<div className="orthodontie-video"> insérer video</div>
 							</div>
 						</Tab.Panel>
 						<Tab.Panel className="custom-tab-panel">
-							<div>
-								<h5>
+							<div className="cards">
+								<h6 className="card">
 									L'orthodontie invisible a débuté il y a environ 20 ans, en
 									plaçant des brackets sur les faces internes des dents . Elle
 									se caractérise par l'existence de brackets spécifiques et la
 									nécessité d'une phase préalable de laboratoire très précise en
 									fonction des malpositions dentaires propres à chaque patient.
-								</h5>
-								<h6>
+									<br />
 									Elle s'adresse à ceux qui refusent un appareil disgracieux,
 									ils peuvent avec cette technique entreprendre un traitement
 									efficace qui leur rendra le sourire sans perturber leur vie
 									professionnelle et sociale
-								</h6>
-								<h5>
+									<br />
 									Pour votre cas, notre équipe s’engage à vous informer sur les
 									avantages et les inconvénients éventuels des traitements à
 									réaliser et sur leurs coûts.
-								</h5>
-								<div className="orthodontie-video"> insérer video</div>
-							</div>
-							<div>
-								<img className="ortho-img" src={orthoLinguale}></img>
+								</h6>
+								<div className="card">
+									<img className="ortho-img" src={orthoLinguale}></img>
+								</div>
 							</div>
 						</Tab.Panel>
 						<Tab.Panel className="custom-tab-panel">
-							<div>
-								<h5>
+							<div className="cards">
+								<h6 className="card">
 									Beaucoup d’adultes qui n’ont pas profité de traitements
 									d’orthodontie quand ils étaient jeunes utilisent cette méthode
 									pour redresser leurs dents. La technique Invisalign® fait
 									appel à des porte-empreintes clairs et amovibles appelés
 									appareils d’alignement.
-								</h5>
-								<h6>
+									<br />
 									Le traitement Invisalign® dure habituellement environ un an
 									mais chaque cas est unique et sa durée dépend à la fois de
 									l’importance de la correction à effectuer et de la fréquence
@@ -115,9 +108,10 @@ const Orthodontie = () => {
 									appareils devraient être portés en tout temps à l’exception
 									des repas et du temps consacré au nettoyage des dents.
 								</h6>
-								<div className="orthodontie-video"> insérer video</div>
+								<div className="card">
+									<img className="ortho-img" src={invisalign}></img>
+								</div>
 							</div>
-							<img className="ortho-img" src={invisalign}></img>
 						</Tab.Panel>
 					</Tab.Panels>
 				</Tab.Group>
