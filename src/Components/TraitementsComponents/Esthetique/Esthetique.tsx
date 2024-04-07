85c9c195986f5e964cc5c8d919967ca01a8cf925
baptiste <baptiste.c.roux@gmail.com>
2024-04-07 20:23:38 +0200

    lol

--- src/Components/TraitementsComponents/Esthetique/Esthetique.tsx
+++ src/Components/TraitementsComponents/Esthetique/Esthetique.tsx
@@ -17,16 +17,16 @@ const Esthetique = () => {
 				<Tab.Group selectedIndex={selectedIndex} onChange={setSelectedIndex}>
 					<Tab.List className="custom-tab-list">
 						<Tab
-							className={`custom-tab ${selectedIndex === 0 ? "active" : ""}`}>
+							className={`custom-tab button-1 ${selectedIndex === 0 ? "active" : ""}`}>
 							Les soins cosmétiques
 						</Tab>
 
 						<Tab
-							className={`custom-tab ${selectedIndex === 1 ? "active" : ""}`}>
+							className={`custom-tab button-1 ${selectedIndex === 1 ? "active" : ""}`}>
 							Le blanchiement dentaire
 						</Tab>
 						<Tab
-							className={`custom-tab ${selectedIndex === 2 ? "active" : ""}`}>
+							className={`custom-tab button-1 ${selectedIndex === 2 ? "active" : ""}`}>
 							Les prothèses dentaies
 						</Tab>
 					</Tab.List>
--- src/Components/shared/Footer.tsx
+++ src/Components/shared/Footer.tsx
@@ -3,7 +3,18 @@ import "./Footer.scss";
 const Footer = () => {
   return (
     <div className="footer-container">
-      <h1>bla bla bli</h1>
+      <div className="top">
+        <h1>Sagesse & Sourire</h1>
+        <div className="telephone">
+          <img src="" alt="" />
+          <h1>02.40.81.01.28</h1>
+        </div>
+        <div className="adresse">
+          <img src="" alt="" />
+          <h1>16, rue du 11 Novembre. 44110 CHATEAUBRIANT</h1>
+        </div>
+      </div>
+      <div className="doctors"></div>
     </div>
   );
 };
